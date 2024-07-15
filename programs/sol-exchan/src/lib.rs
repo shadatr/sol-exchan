296c5f0e695fdf9c85acbc572042f63778afaf91
shdab <130516923+shadatr@users.noreply.github.com>
2024-07-15 23:16:54 +0300

    .

--- programs/sol-exchan/src/lib.rs
+++ programs/sol-exchan/src/lib.rs
@@ -1,15 +1,43 @@
+pub mod ray_token;
+
 use anchor_lang::prelude::*;
 
 declare_id!("5umccz9MSnMxV2UDbQWEoxarCk7UFaipFkcgzW5ggKs2");
 
-#[program]
+// #[program]
 pub mod sol_exchan {
-    use super::*;
+    // use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
+    // pub fn handle_sol_transfer(ctx: Context<HandleSolTransfer>) -> Result<()> {
+    //     // Assuming the SOL transfer already happened, we transfer the token
+    //     let ix = transfer(
+    //         &spl_token::ID,
+    //         &ctx.accounts.from_token_account.key(),
+    //         &ctx.accounts.to_token_account.key(),
+    //         &ctx.accounts.token_authority.key(),
+    //         &[],
+    //         1, // Number of tokens to transfer
+    //     )?;
+    //     anchor_lang::solana_program::program::invoke(
+    //         &ix,
+    //         &[
+    //             ctx.accounts.from_token_account.to_account_info(),
+    //             ctx.accounts.to_token_account.to_account_info(),
+    //             ctx.accounts.token_authority.to_account_info(),
+    //             ctx.accounts.token_program.to_account_info(),
+    //         ],
+    //     )?;
+    //     Ok(())
+    // }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+// #[derive(Accounts)]
+// pub struct HandleSolTransfer<'info> {
+//     #[account(mut)]
+//     pub from_token_account: AccountInfo<'info>,
+//     #[account(mut)]
+//     pub to_token_account: AccountInfo<'info>,
+//     #[account(signer)]
+//     pub token_authority: AccountInfo<'info>,
+//     pub token_program: AccountInfo<'info>,
+// }
