5332628bc7b5848e2d8302b6b04dcaa9411dcacc
shdab <130516923+shadatr@users.noreply.github.com>
2024-07-16 22:33:32 +0300

    .

--- programs/sol-exchan/src/lib.rs
+++ programs/sol-exchan/src/lib.rs
@@ -1,43 +1,35 @@
-pub mod ray_token;
-
+pub mod buy;
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Token, TokenAccount, Transfer};
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("5umccz9MSnMxV2UDbQWEoxarCk7UFaipFkcgzW5ggKs2");
 
-// #[program]
+#[program]
 pub mod sol_exchan {
-    // use super::*;
+    use super::*;
 
-    // pub fn handle_sol_transfer(ctx: Context<HandleSolTransfer>) -> Result<()> {
-    //     // Assuming the SOL transfer already happened, we transfer the token
-    //     let ix = transfer(
-    //         &spl_token::ID,
-    //         &ctx.accounts.from_token_account.key(),
-    //         &ctx.accounts.to_token_account.key(),
-    //         &ctx.accounts.token_authority.key(),
-    //         &[],
-    //         1, // Number of tokens to transfer
-    //     )?;
-    //     anchor_lang::solana_program::program::invoke(
-    //         &ix,
-    //         &[
-    //             ctx.accounts.from_token_account.to_account_info(),
-    //             ctx.accounts.to_token_account.to_account_info(),
-    //             ctx.accounts.token_authority.to_account_info(),
-    //             ctx.accounts.token_program.to_account_info(),
-    //         ],
-    //     )?;
-    //     Ok(())
-    // }
+    pub fn buy_token(ctx: Context<BuyToken>, amount: u64) -> ProgramResult {
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.user_token_account.to_account_info().clone(),
+            to: ctx.accounts.destination_account.to_account_info().clone(),
+            authority: ctx.accounts.user_authority.to_account_info().clone(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info().clone();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        token::transfer(cpi_ctx, amount)?;
+
+        Ok(())
+    }
 }
 
-// #[derive(Accounts)]
-// pub struct HandleSolTransfer<'info> {
-//     #[account(mut)]
-//     pub from_token_account: AccountInfo<'info>,
-//     #[account(mut)]
-//     pub to_token_account: AccountInfo<'info>,
-//     #[account(signer)]
-//     pub token_authority: AccountInfo<'info>,
-//     pub token_program: AccountInfo<'info>,
-// }
+#[derive(Accounts)]
+pub struct BuyToken<'info> {
+    #[account(mut)]
+    pub user_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub destination_account: Account<'info, TokenAccount>,
+    pub user_authority: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+}
