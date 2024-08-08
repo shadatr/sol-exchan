ed6f55e0b1c2913d3df47c0d0f1846ddad302196
shdab <130516923+shadatr@users.noreply.github.com>
2024-08-08 10:13:36 +0300

    .

--- frontend/app/tokens/discover/page.tsx
+++ frontend/app/tokens/discover/page.tsx
@@ -2,48 +2,38 @@
 import SidebarPages from "@/components/sidebar-pages";
 import React, { useEffect, useState } from "react";
 import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
-import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 // Constants
 const PROGRAM_ID = new PublicKey(
-  "HRfR1RiRCLHmfgG5SCmgXrBvdpCYtfGc8fCJSrEXej6W"
+  "CPMDWBwJDtYax9qW7AyRuVC19Cc4L4Vcy4n2BHAbHkCW"
 ); // Your program ID
-const RPC_ENDPOINT =
-  "https://api.devnet.solana.com"; // RPC endpoint for the Solana network
+const RPC_ENDPOINT = "https://api.devnet.solana.com"; // RPC endpoint for the Solana network
 
-  
-  const connection = new Connection(RPC_ENDPOINT, 'confirmed');
+const connection = new Connection(RPC_ENDPOINT, "confirmed");
 const Page = () => {
-
   const [tokenAccounts, setTokenAccounts] = useState<any[]>([]);
 
   useEffect(() => {
     const getTokens = async () => {
       console.log("Getting token accounts...");
       try {
-      
- 
-        //     const tokenAccounts1 = await connection.getProgramAccounts(
-        //       PROGRAM_ID,
-        //   // { programId:TOKEN_PROGRAM_ID } // Replace with actual Token Program ID
-        // );
-    
-        // console.log("Token accounts:", tokenAccounts[0].pubkey.toBase58());
         const tokenAccounts = await connection.getParsedTokenAccountsByOwner(
           PROGRAM_ID,
-      { programId:TOKEN_PROGRAM_ID } // Replace with actual Token Program ID
-    );
+          { programId: TOKEN_PROGRAM_ID } 
+        );
 
-    console.log("Token accounts:", tokenAccounts);
+        console.log("Token accounts:", tokenAccounts);
 
-    const tokens = tokenAccounts.value.map((account) => {
-      const tokenMint = account.account.data.parsed.info.mint;
-      const tokenAmount = account.account.data.parsed.info.tokenAmount.uiAmount;
-      return { tokenMint, tokenAmount };
-    });
+        const tokens = tokenAccounts.value.map((account) => {
+          const tokenMint = account.account.data.parsed.info.mint;
+          const tokenAmount =
+            account.account.data.parsed.info.tokenAmount.uiAmount;
+          return { tokenMint, tokenAmount };
+        });
 
-    console.log('Tokens in pool:', tokens);
-    return tokens;
+        console.log("Tokens in pool:", tokens);
+        return tokens;
       } catch (error) {
         console.log("Error getting token accounts:", error);
       }
@@ -55,7 +45,7 @@ const Page = () => {
   return (
     <div>
       <SidebarPages />
-      <div>{tokenAccounts.length}</div>      
+      <div>{tokenAccounts.length}</div>
     </div>
   );
 };
