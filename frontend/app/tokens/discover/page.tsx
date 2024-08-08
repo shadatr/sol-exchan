754752fe3f41e3d282ef32536100ac7ce66d5989
shdab <130516923+shadatr@users.noreply.github.com>
2024-08-08 10:13:52 +0300

    .

--- frontend/app/tokens/discover/page.tsx
+++ frontend/app/tokens/discover/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import SidebarPages from "@/components/sidebar-pages";
 import React, { useEffect, useState } from "react";
-import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { Connection, PublicKey } from "@solana/web3.js";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 // Constants
