5a18d145c2b6225771640ac72c6c35627e81c107
shdab <130516923+shadatr@users.noreply.github.com>
2024-08-25 21:24:34 +0300

    .

--- frontend/app/tokens/discover/page.tsx
+++ frontend/app/tokens/discover/page.tsx
@@ -11,7 +11,7 @@ const PROGRAM_ID = new PublicKey(
 const RPC_ENDPOINT = "https://api.devnet.solana.com"; 
 const connection = new Connection(RPC_ENDPOINT, "confirmed");
 const Page = () => {
-  
+
   const [tokenAccounts, setTokenAccounts] = useState<any[]>([]);
 
   useEffect(() => {
